c4782dedf19544d433e790d8ec018ed28937da03
Taher <mo7amed_taher@hotmail.com>
2019-02-01 19:17:20 +0300

    Add project files.

---
+++ LiveConcerts/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LiveConcerts.Startup))]
+namespace LiveConcerts
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
